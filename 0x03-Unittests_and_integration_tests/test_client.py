496cf7959d88af6d2ffd26d69d2e76283c6ff96c
Dama5323 <your-deenyashke@gmail.com>
2025-05-29 14:40:39 +0300

    test(client): add unit test for public_repos method

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -25,9 +25,8 @@ class TestGithubOrgClient(unittest.TestCase):
         client = GithubOrgClient(org_name)
         result = client.org
         
-        mock_get_json.assert_called_once_with(
-            f"https://api.github.com/orgs/{org_name}"
-        )
+        url = f"https://api.github.com/orgs/{org_name}"
+        mock_get_json.assert_called_once_with(url)
         self.assertEqual(result, expected_response)
 
     def test_public_repos_url(self):
