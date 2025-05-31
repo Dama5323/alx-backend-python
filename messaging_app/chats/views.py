1cb71dcde753b09097f57c46fec3fd53a11e0a6b
Dama5323 <your-deenyashke@gmail.com>
2025-05-31 15:00:54 +0300

    feat(api): add viewsets for conversations and messages with create and list endpoints

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,8 +1,39 @@
-# Example view usage
-from rest_framework import generics
-from .models import Conversation
-from .serializers import ConversationSerializer
+from rest_framework import viewsets, permissions
+from .models import Conversation, Message
+from .serializers import (
+    ConversationSerializer,
+    ConversationCreateSerializer,
+    MessageSerializer,
+    MessageCreateSerializer
+)
 
-class ConversationDetailView(generics.RetrieveAPIView):
+class ConversationViewSet(viewsets.ModelViewSet):
     queryset = Conversation.objects.all()
-    serializer_class = ConversationSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_serializer_class(self):
+        if self.action == 'create':
+            return ConversationCreateSerializer
+        return ConversationSerializer
+
+    def perform_create(self, serializer):
+        serializer.save()
+
+    def get_queryset(self):
+        return Conversation.objects.filter(participants=self.request.user)
+
+
+class MessageViewSet(viewsets.ModelViewSet):
+    queryset = Message.objects.all()
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_serializer_class(self):
+        if self.action == 'create':
+            return MessageCreateSerializer
+        return MessageSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(sender=self.request.user)
+
+    def get_queryset(self):
+        return Message.objects.filter(conversation__participants=self.request.user)
