4aa8a939f0975789ddd5f0acdd704cdd7b8182d0
Dama5323 <your-deenyashke@gmail.com>
2025-05-31 15:13:47 +0300

    feat(api): add viewsets for conversations and messages with create and list endpoints

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,5 +1,9 @@
-from rest_framework import viewsets, permissions
-from .models import Conversation, Message
+from rest_framework import viewsets, status, filters
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import action
+
+from .models import Conversation, Message, User
 from .serializers import (
     ConversationSerializer,
     ConversationCreateSerializer,
@@ -7,33 +11,38 @@ from .serializers import (
     MessageCreateSerializer
 )
 
+
 class ConversationViewSet(viewsets.ModelViewSet):
     queryset = Conversation.objects.all()
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [IsAuthenticated]
+    filter_backends = [filters.OrderingFilter]
+    ordering = ['-updated_at']
 
     def get_serializer_class(self):
         if self.action == 'create':
             return ConversationCreateSerializer
         return ConversationSerializer
 
-    def perform_create(self, serializer):
-        serializer.save()
-
     def get_queryset(self):
         return Conversation.objects.filter(participants=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save()
 
 
 class MessageViewSet(viewsets.ModelViewSet):
     queryset = Message.objects.all()
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [IsAuthenticated]
+    filter_backends = [filters.OrderingFilter]
+    ordering = ['sent_at']
 
     def get_serializer_class(self):
         if self.action == 'create':
             return MessageCreateSerializer
         return MessageSerializer
 
-    def perform_create(self, serializer):
-        serializer.save(sender=self.request.user)
-
     def get_queryset(self):
         return Message.objects.filter(conversation__participants=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(sender=self.request.user)
