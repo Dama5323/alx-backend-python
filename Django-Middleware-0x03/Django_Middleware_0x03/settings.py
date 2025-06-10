ce710bab1d9d16831829462d05c1cc973ef155cd
Dama5323 <your-deenyashke@gmail.com>
2025-06-10 07:45:41 +0300

    feat: add RequestLoggingMiddleware to track user requests

--- Django-Middleware-0x03/Django_Middleware_0x03/settings.py
+++ Django-Middleware-0x03/Django_Middleware_0x03/settings.py
@@ -40,11 +40,11 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'chats.middleware.RolePermissionMiddleware',  
+    'chats.middleware.RequestLoggingMiddleware',  
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'chats.middleware.RequestLoggingMiddleware',
     'chats.middleware.RestrictAccessByTimeMiddleware',
+    'chats.middleware.RolePermissionMiddleware',
     # 'chats.middleware.OffensiveLanguageMiddleware',  
 ]
 
